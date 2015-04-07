7fd3a3979e199920b79387975fab7418a9754d3a
Cyberarm <matthewlikesrobots@gmail.com>
2015-04-07 17:49:56 -0500

    Dynamic tail lights, less flashy head lights.

--- lib/car/car.rb
+++ lib/car/car.rb
@@ -1,5 +1,5 @@
 class Car < Chingu::GameObject
-  attr_reader :speed
+  attr_reader :speed, :braking
 
   def setup
     self.zorder = 5
@@ -10,7 +10,7 @@ class Car < Chingu::GameObject
     @physics = Car::Physics.new(self)
 
     @debug = Game::Text.new("", size: 50)
-    @name  = Game::Text.new("#{@car_data["name"]}", size: 30)
+    @name  = Game::Text.new("#{@car_data["name"]}", size: 20)
 
     @speed = 0.0
 
@@ -20,6 +20,8 @@ class Car < Chingu::GameObject
 
     @braking = false
     @tick = 0
+    @yellow_up = false
+    @yellow_int = 255
   end
 
   def draw
@@ -27,9 +29,10 @@ class Car < Chingu::GameObject
     @debug.draw
     @name.draw
 
-    # Do some kind of transformation to rotate in sync with car
+    # Does some kind of transformation to rotate in sync with car
     $window.rotate(self.angle, self.x, self.y) do
-      _yellow = Gosu::Color.rgb(rand(255), rand(255), 0) # Flicker
+      # TODO: fade between 2 colors instead of using Random
+      _yellow = Gosu::Color.rgb(@yellow_int, @yellow_int, 0)
       @car_data["spec"]["lights"]["head_lights"].each do |light|
         $window.fill_rect([(self.x-(self.width/2))+light["left"],
                            (self.y-(self.height/2))+light["top"],
@@ -37,7 +40,17 @@ class Car < Chingu::GameObject
                            light["height"]], _yellow, 6)
       end
 
-      _red = Gosu::Color.rgb(rand(255), 0, 0) # Flicker
+      if @braking
+        # Braking
+        _red = Gosu::Color.rgb(255, 0, 0)
+      elsif @speed <= -0.001
+        # Backup lights
+        _red = Gosu::Color.rgb(255, 255, 255)
+      else
+        # Not braking
+        _red = Gosu::Color.rgb(140, 0, 0)
+      end
+
       @car_data["spec"]["lights"]["tail_lights"].each do |light|
         $window.fill_rect([(self.x-(self.width/2))+light["left"],
                            (self.y-(self.height/2))+light["top"],
@@ -51,16 +64,33 @@ class Car < Chingu::GameObject
     super
     @tick+=1
 
+    if @yellow_up
+      if @yellow_int >= 255
+        @yellow_up = false
+      else
+        @yellow_int+=1
+      end
+    else
+      if @yellow_int <= 190
+        @yellow_up = true
+      else
+        @yellow_int-=1
+      end
+    end
+
     @debug.text = "Angle:#{self.angle.round(1)} Speed:#{@speed.round(1)} Pixels Per Frame - FPS:#{Gosu.fps}"
-    @name.x,@name.y = self.x-@name.width/2,self.y-@name.height
+    @name.x,@name.y = self.x-@name.width/2,self.y-self.height*self.factor-24
     @physics.update
 
     unless @speed >= @top_speed
+      @braking = false
+
       if holding?(:up)
-        if @speed <= -0.1
+        if @speed <= -0.01
           @braking = true
           @speed+=@break_speed
         else
+          @braking = false
           @speed+=@top_speed/100.0
         end
       end
@@ -68,10 +98,11 @@ class Car < Chingu::GameObject
 
     unless @speed <= -@top_speed
       if holding?(:down)
-        if @speed >= 0.1
+        if @speed >= 0.01
           @speed-=@break_speed
           @braking = true
         else
+          @braking = false
           @speed-=@top_speed/100.0
         end
       end
