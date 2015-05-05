8bfd39aad52dfe57cf435ad6ed99c3f7de0e9d36
Cyberarm <matthewlikesrobots@gmail.com>
2015-05-04 21:00:44 -0500

    Added skeleton net client, updated Pause to properly render menu.

---
+++ lib/net/client.rb
@@ -0,0 +1,11 @@
+# module Game
+#   module Net
+#     class Client < GameOverseer::Client
+#       # client = Game::Net::Client.new(host, port)
+#       # client.connect
+#       # client.send(:channel, {data}, :reliable)
+#       # client.handle_packet(:method)
+#       # client.disconnect
+#     end
+#   end
+# end
--- lib/scenes/pause.rb
+++ lib/scenes/pause.rb
@@ -2,19 +2,24 @@ module Game
   class Scene
     class Pause < Menu
       def prepare
-        @last_state = @options[:last_state]
         title("Ruby Sync Racing")
         label("Paused", size: 50)
+
+        button("Return to Game") do
+          $window.show_cursor = false
+          push_game_state(@options[:last_state], setup: false)
+        end
+
         button("Main Menu") do
           push_game_state(MainMenu)
         end
       end
 
       def draw
-        @last_state.draw
+        @options[:last_state].draw
         $window.flush
-        super
         fill(Gosu::Color.rgba(10,15,20,200))
+        super
       end
 
       def button_up(id)
@@ -23,9 +28,7 @@ module Game
         case id
         when Gosu::KbEscape
           $window.show_cursor = false
-          push_game_state(@last_state, setup: false)
-        when Gosu::KbM
-          push_game_state(MainMenu)
+          push_game_state(@options[:last_state], setup: false)
         end
       end
     end
