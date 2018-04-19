0fa3f9e0ca6dab101483c826dcaa702c6bcc750a
Cyberarm <matthewlikesrobots@gmail.com>
2018-04-19 08:34:04 -0500

    Lets play with time xD

--- lib/engine.rb
+++ lib/engine.rb
@@ -2,6 +2,6 @@ require_all Dir.glob("lib/**/*.rb").reject { |f| f.include?("server/") }
 
 module Engine
   def self.timestamp
-    Time.now.to_f*1000.0
+    Process.clock_gettime(Process::CLOCK_MONOTONIC, :float_millisecond)
   end
 end
