3012e77aa2ae203c91943b36f04b1b2caa95c20a
Cyberarm <matthewlikesrobots@gmail.com>
2018-06-18 21:14:09 -0500

    Replaced car selection with newer model

--- lib/scenes/car_selection.rb
+++ lib/scenes/car_selection.rb
@@ -2,9 +2,6 @@ module Game
   class Scene
     class CarSelection < Menu
       def prepare
-        # List cars graphically
-        # with top speed, .. ..
-        # push_game_state(Game)
         title("Ruby Sync Racing")
         label("Choose Car", size: 50)
         button("← Track Selection") {push_game_state(LevelSelection)}
@@ -12,51 +9,83 @@ module Game
 
         @cars = Dir.glob("data/cars/*.json")
         @car_list = []
-
+        @list_index = 0
         process_cars
+
+        @color_options = [Gosu::Color::RED, Gosu::Color::GREEN, Gosu::Color::BLUE, Gosu::Color::BLACK]
+        @multiline_text = MultiLineText.new("0\n1\n2\n", x: $window.width/2, y: 280, size: 18)
       end
 
       def process_cars
-        @cars.each_with_index do |car, i|
-          _car = AbstractJSON.load(open(car).read)
-
-          info = "Top speed: #{_car["spec"]["top_speed"]}, Break speed: #{_car["spec"]["break_speed"]}, Drag: #{_car["spec"]["drag"]}"
-
-          button(_car["name"]) {
-            push_game_state(Play.new(trackfile: options[:trackfile], carfile: @cars[i]))
-          }
-          label(info)
-          label("", size: 15)
+        @cars.each do |car|
+          data = AbstractJSON.load(File.open(car).read)
+          hash = {}
+          hash[:data]        = data
+          hash[:json]        = car
+          hash[:image]       = image(AssetManager.image_from_id(data["spec"]["image"]))
+          hash[:body_image]  = image(AssetManager.image_from_id(data["spec"]["body_image"]))
+          hash[:color]       = Gosu::Color::WHITE
+          hash[:top_speed]   = data["spec"]["top_speed"]
+          hash[:break_speed] = data["spec"]["break_speed"]
+          hash[:drag]        = data["spec"]["drag"]
+          hash[:scale]       = data["spec"]["scale"]
+          @car_list << hash
         end
       end
 
-      # def draw
-      #   super
-      #   fill(Gosu::Color.rgba(45,45,76,89))
-      #
-      #   @car_list.each do |car|
-      #     $window.fill_rect([car["text"].x-4,car["text"].y-4, car["text"].width+8, car["text"].height+4], Gosu::Color.rgba(0,0,0,200), 1)
-      #
-      #     if $window.mouse_x.between?(car["text"].x-4, car["text"].x+car["text"].width+8)
-      #       if $window.mouse_y.between?(car["text"].y-4, car["text"].y+car["text"].height+4)
-      #         $window.fill_rect([car["text"].x-4,car["text"].y-4, car["text"].width+8, car["text"].height+4], Gosu::Color::GRAY, 1)
-      #       end
-      #     end
-      #
-      #     car["text"].draw
-      #     car["info"].draw
-      #     car["image"].draw_rot(car["text"].x-30, car["text"].y-4, 2, -90, 1,1, car["spec"]["factor"], car["spec"]["factor"])
-      #   end
-      # end
+      def draw
+        super
+        Gosu.draw_rect($window.width/2-100, 280, 200, 64+15, Gosu::Color.rgb(50,50,100))
+        list = @car_list[@list_index]
+        list[:image].draw($window.width/2-85, 290, 3, list[:scale], list[:scale])
+        list[:body_image].draw($window.width/2-85, 290, 3, list[:scale], list[:scale], list[:color])
+        @multiline_text.draw
+
+        @color_options.each_with_index do |color, i|
+          Gosu.draw_rect($window.width/2-100+(i*20), 280+64+15, 20, 20, color)
+        end
+      end
+
+      def update
+        super
+        list = @car_list[@list_index]
+
+        @multiline_text.text = "Speed: #{list[:top_speed]}\nBrake: #{list[:break_speed]}\nDrag: #{list[:drag]}\n"
+      end
+
+      def mouse_over?
+        if $window.mouse_x.between?($window.width/2-100, $window.width/2-100+200)
+          if $window.mouse_y.between?(280, 280+64+15)
+            true
+          end
+        end
+      end
+
+      def continue_to_game
+        list = @car_list[@list_index]
+        push_game_state(Play.new(trackfile: @options[:trackfile], carfile: list[:json]))
+      end
 
       def button_up(id)
         super
-        @car_list.each_with_index do |car, i|
-          if $window.mouse_x.between?(car["text"].x-4, car["text"].x+car["text"].width+8)
-            if $window.mouse_y.between?(car["text"].y-4, car["text"].y+car["text"].height+4)
-              # Do Stuff
-              push_game_state(Play.new(trackfile: options[:trackfile], carfile: @cars[i]))
-            end
+        case id
+        when Gosu::MsLeft
+          if mouse_over?
+            continue_to_game
+          end
+        when Gosu::KbReturn
+          continue_to_game
+        when Gosu::KbEnter
+          continue_to_game
+        when Gosu::KbLeft
+          @list_index-=1
+          if @list_index < 0
+            @list_index = @car_list.count-1
+          end
+        when Gosu::KbRight
+          @list_index+=1
+          if @list_index > @car_list.count-1
+            @list_index = 0
           end
         end
       end
